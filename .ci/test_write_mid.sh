20cbfe061b0e6b1232fbec1a46b62233c8afa9b7
RinHizakura <s921975628@gmail.com>
2022-01-03 22:36:29 +0800

    Add write middle size file test

---
+++ .ci/test_write_mid.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PARENT_SRC_FILE="../src/chisai-format.c"
+RESULT_FILE="a.txt"
+
+cat ${PARENT_SRC_FILE} > ${RESULT_FILE}
+diff ${PARENT_SRC_FILE} ${RESULT_FILE}
+if [ $? != 0 ]; then
+    exit 1
+fi
+exit 0
