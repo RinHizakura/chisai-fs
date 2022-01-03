49a8a58d17e0430246129b3590da7330c345dd8d
RinHizakura <s921975628@gmail.com>
2022-01-04 00:40:02 +0800

    Delete the output of diff if there are any

--- .ci/test_write.sh
+++ .ci/test_write.sh
@@ -4,7 +4,7 @@ PARENT_SRC_FILE="../src/chisai-format.c"
 RESULT_FILE="a.txt"
 
 cat ${PARENT_SRC_FILE} > ${RESULT_FILE}
-diff ${PARENT_SRC_FILE} ${RESULT_FILE}
+diff ${PARENT_SRC_FILE} ${RESULT_FILE} > /dev/null
 if [ $? != 0 ]; then
     exit 1
 fi
